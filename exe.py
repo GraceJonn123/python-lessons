34de2ba89df31cc5af2fb47d027364758c70697d
GraceJonn123 <gj.gjonn@gmail.com>
2016-01-31 11:18:10 +0300

    all files in python-lessons

---
+++ exe.py
@@ -0,0 +1,18 @@
+print "Hello World"
+print "Hello Again"
+print "I like typing this" # Although this is very unnecessary jo
+#Who does that really
+#really doesnt make sense
+print "This is fun"
+print "Yay! Printing" #IKR
+# what does this mean ata print "I'd much rather you 'not'"
+print "It takes a while though"
++plus
+-minus
+/ slash
+*asterisk
+%percent
+<less than
+>greater than
+<=less than equal
+>=greater than equal
