b708dff42d3917673009a519d537397e02b723b0
GraceJonn123 <gj.gjonn@gmail.com>
2016-02-15 01:53:24 +0300

    generating prime numbers to 100

--- generator.py
+++ generator.py
@@ -1,8 +1,11 @@
+#a full programme that introduces a prime number
 def isprime(w):
     if w == 1:
+        #one is not prime
         return False
     for x in range (2, w):
         if w % x == 0:
+            #prime numbers arent divisible by two
             return False
     else:
         return True
@@ -13,5 +16,6 @@ def primes(w = 1):
         w += 1
 for w in primes():
     if w > 100: break
+    #stops looping at 100
     print (w)
 
