3f23c43a7305811b3e6c3d3e0193d4bd841c0aac
GraceJonn123 <gj.gjonn@gmail.com>
2016-02-15 02:28:10 +0300

    conditionals and elif

---
+++ math2.py
@@ -0,0 +1,11 @@
+def main():
+	a, b = 79, 78#these are variables(assignment)
+	if a > b:#this is a conditional
+		#this colon means there will be a line of code indented under it.
+		print("a is greater than b")
+	elif a < b:
+		print("a is less than b")
+	else:
+		print("a is equal to b")
+
+if __name__ == "__main__": main()
