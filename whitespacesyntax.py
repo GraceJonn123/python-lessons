3f797a0ac636cf8b0373267e4bdab41202a3102d
GraceJonn123 <gj.gjonn@gmail.com>
2016-02-15 02:16:36 +0300

    whitespaces in python

---
+++ whitespacesyntax.py
@@ -0,0 +1,6 @@
+def main():
+	print("I am learning whitespace.")
+print("Whitespaces are fun")
+
+if __name__ =="__main__": main()
+#the latter will be printed because it is not associated with the main function.
