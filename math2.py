f8c52d2a8367d14c4f8d2106d3da78577f0674da
GraceJonn123 <gj.gjonn@gmail.com>
2016-02-15 10:30:05 +0300

    all content

--- math2.py
+++ math2.py
@@ -15,4 +15,5 @@ if __name__ == "__main__": main()
 #def main():
 #	if a, b = 5, 8
 #	s = "less than" if a < b else "not less than"
-#
+#print(s)
+#if __name__ == "__main__": main()
---
+++ oop.py
@@ -0,0 +1,11 @@
+class Egg:
+	def __init__(self, kind = "fried"):
+		self.kind = kind
+
+	def whatkind(self):
+		return self.kind
+def main():
+	fried = Egg
+	scrambled = Egg("scrambled")
+print (self.kind)
+if __name__ == "__main__": main()
