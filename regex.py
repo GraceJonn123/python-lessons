5f1846d808af401d8f7ba90967a7a7318fb6918e
GraceJonn123 <gj.gjonn@gmail.com>
2016-02-15 11:18:57 +0300

    regex

---
+++ regex.py
@@ -0,0 +1,8 @@
+import re
+
+def main():
+    fh - open('sparrow.text')
+    for line in fh:
+        if re.search('sparrow', line):
+            print(line, end=' ')
+if __name__ == "__main__":main()
