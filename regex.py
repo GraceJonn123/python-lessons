f699f8cd82498a47e3403ecb81b18168a828489b
GraceJonn123 <gj.gjonn@gmail.com>
2016-02-15 11:25:19 +0300

    matching regex

--- regex.py
+++ regex.py
@@ -2,7 +2,10 @@ import re
 
 def main():
     fh = open('sparrow.text')
+    #opens the text sparrow and reads it line by line
     for line in fh:
-        if re.search('sparrow', line):
-            print(line, end=' ')
+        match = re.search('sparrow', line)
+        if match:
+            #regex search...searching for regular expressions
+            print(match.group())
 if __name__ == "__main__":main()
